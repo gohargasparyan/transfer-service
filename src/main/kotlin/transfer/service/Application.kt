ad15a447b463261e4a03d32beb672dd7e65986fd
gohargasparyan <gohar.s.gasparyan@gmail.com>
2020-01-18 20:38:44 +0100

    Init Server App with Micronaut CLI, language kotlin, build with gradle

---
+++ src/main/kotlin/transfer/service/Application.kt
@@ -0,0 +1,14 @@
+package transfer.service
+
+import io.micronaut.runtime.Micronaut
+
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("transfer.service")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
